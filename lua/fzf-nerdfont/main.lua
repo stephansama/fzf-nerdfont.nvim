6131ec627e861f302db7bc5c068f7073cdef17fa
stephansama <stephanrandle@hotmail.com>
2025-10-09 04:59:35 -0400

    feat: removed state check
    Co-authored-by: Stephan Randle <stephanrandle@hotmail.com>
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/fzf-nerdfont/main.lua
+++ lua/fzf-nerdfont/main.lua
@@ -1,21 +1,20 @@
 local log = require("fzf-nerdfont.util.log")
-local state = require("fzf-nerdfont.state")
 
 -- internal methods
 ---@class FzfNerdFont.Main
 local Main = {}
 
+local _unpack = unpack or table.unpack
+
 ---@param selected string[]
 local function set_icon(selected)
-    local _unpack = unpack or table.unpack
-
     local original_buf = vim.api.nvim_get_current_buf()
     local original_win = vim.api.nvim_get_current_win()
 
     for _, f in ipairs(selected) do
         local row, col = _unpack(vim.api.nvim_win_get_cursor(original_win)) -- Get current cursor position
-        local icon = f:match("^(%S+)")
         local line = _unpack(vim.api.nvim_buf_get_lines(original_buf, row - 1, row, false))
+        local icon = f:match("^(%S+)")
         local text = line:sub(1, col) .. icon .. line:sub(col + 1)
         local new_line = { text }
         vim.api.nvim_buf_set_lines(original_buf, row - 1, row, true, new_line)
@@ -29,17 +28,12 @@ end
 --- @param scope? string: internal identifier for logging purposes.
 function Main.run(scope)
     scope = scope or ""
-    if state.enabled then
-        log.debug(scope, "fzf-nerdfont is already enabled")
-        return
-    end
 
     local script_path = debug.getinfo(1, "S").source:sub(2)
     local script_dir = vim.fn.fnamemodify(script_path, ":h")
     -- TODO: (DrKJeff16) Need a better file location
     local glyphs = vim.fn.readfile(script_dir .. "/glyphnames")
 
-    state.enabled = true
     log.debug(scope, "fzf-nerdfont enabled")
 
     require("fzf-lua").fzf_exec(glyphs, {
