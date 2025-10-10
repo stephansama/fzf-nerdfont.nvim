040716f569384934625e0c5545cc810229036c0b
stephansama <stephanrandle@hotmail.com>
2025-10-09 20:55:49 -0400

    chore: run setup if it somehow was not invoked
    Co-authored-by: stephansama <stephanrandle@hotmail.com>
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/fzf-nerdfont/init.lua
+++ lua/fzf-nerdfont/init.lua
@@ -4,21 +4,21 @@ local config = require("fzf-nerdfont.config")
 ---@class FzfNerdFont
 local FzfNerdFont = {}
 
+-- setup FzfNerdFont options and merge them with user provided ones.
+FzfNerdFont.setup = config.setup
+
 --- Toggle the plugin by calling the `enable`/`disable` methods respectively.
 function FzfNerdFont.run()
     if vim.g.fzf_nerd_font_setup ~= 1 then
-        return
+        FzfNerdFont.setup()
     end
 
     main.run("public_api_toggle")
 end
 
--- setup FzfNerdFont options and merge them with user provided ones.
-FzfNerdFont.setup = config.setup
-
 function FzfNerdFont.generate()
     if vim.g.fzf_nerd_font_setup ~= 1 then
-        return
+        FzfNerdFont.setup()
     end
 
     main.generate()
