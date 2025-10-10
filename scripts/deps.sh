aba560f7f7cc55e3c6b3e798aadf3acf744714bc
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-10 14:44:11 -0600

    fix(scripts): restored removal instruction
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -6,23 +6,24 @@ DEP="$1"
 LUALS_URL="https://github.com/LuaLS/lua-language-server/releases/download/3.7.4/lua-language-server-3.7.4-darwin-x64.tar.gz"
 
 __get_mini() {
-	if ! [ -d ./deps/mini.nvim ]; then
+    if ! [ -d ./deps/mini.nvim ]; then
         printf "git clone --depth 1 https://github.com/nvim-mini/mini.nvim deps/mini.nvim\n"
-	    git clone --depth 1 https://github.com/nvim-mini/mini.nvim deps/mini.nvim || return 1
-	fi
-	return 0
+        git clone --depth 1 https://github.com/nvim-mini/mini.nvim deps/mini.nvim || return 1
+    fi
+    return 0
 }
 
 __get_lua_ls() {
     if [[ -d .ci/lua-ls ]]; then
         printf "rm -rf .ci/lua-ls/log\n"
+        rm -rf .ci/lua-ls/log
         return 0
     fi
 
 	printf "%s\n" "mkdir -p .ci/lua-ls" "curl -sL \"\$LUALS_URL\" | tar xzf - -C \"\$(pwd)/.ci/lua-ls\""
 
     mkdir -p .ci/lua-ls
-	curl -sL "$LUALS_URL" | tar xzf - -C "$(pwd)/.ci/lua-ls"
+    curl -sL "$LUALS_URL" | tar xzf - -C "$(pwd)/.ci/lua-ls"
 
     return $?
 }
