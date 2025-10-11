31d533c955d09cea82f108b69c6f48cdec79433b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-11 12:57:59 -0600

    build(deps): restored return statement
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -8,7 +8,7 @@ LUALS_URL="https://github.com/LuaLS/lua-language-server/releases/download/3.7.4/
 __get_mini() {
     if ! [ -d ./deps/mini.nvim ]; then
         printf "git clone --depth 1 https://github.com/nvim-mini/mini.nvim deps/mini.nvim\n"
-        git clone --depth 1 https://github.com/nvim-mini/mini.nvim deps/mini.nvim
+        git clone --depth 1 https://github.com/nvim-mini/mini.nvim deps/mini.nvim || return 1
     fi
     return 0
 }
